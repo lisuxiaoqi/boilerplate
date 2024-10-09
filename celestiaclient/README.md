5180fa728c75e1deafa1e392bd66b5d94e244d73
lisuxiaoqi <lisuxiaoqi@gmail.com>
2024-10-09 09:32:54 +0800

    update

---
+++ celestiaclient/README.md
@@ -0,0 +1,5 @@
+## 说明
+本项目是关于tendermint client的模版。
+
+文档： https://docs.tendermint.com/v0.34/rpc/
+
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -2,7 +2,10 @@ const { ethers } = require("hardhat");
 const web3 = require('web3');
 async function deploy(deployer) {
   const Contract = await ethers.getContractFactory("Sample", deployer);
-  const contract = await Contract.deploy();
+  const contract = await Contract.deploy({
+      'maxPriorityFeePerGas':1000000000,     //1gwei here, 1*10^9 ether(1nano) gas tip. gas price wei
+        'maxFeePerGas':50000000000,    //50gwei here. gas cap, gas price. wei. gate requires at least 5gwei
+  });
   await contract.deployed();
   console.log("Contract deployed to address:", contract.address);
   return contract.address;
